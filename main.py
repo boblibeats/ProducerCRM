f60205de0733b8cc8e5e450c9c17a4d0c36e1b1a
Bob Li <bobli@Mac.lan>
2026-06-29 18:12:35 -0400

    Add POST /artists endpoint with Pydantic validation

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class ArtistCreate(BaseModel):
+    name = str
+    genre = str | None = None
 
 fake_artists = [
     {"id": 1, "name": "Sawyer Utah", "genre": "Indie Pop"},
@@ -21,3 +26,13 @@ def get_artists(artist_id:int):
         if artist["id"] == artist_id:
             return artist
     return {"error": "artist not found"}
+
+@app.post("/artists")
+def create_artist(artist: ArtistCreate):
+    new_artist = {
+        "id" : len(fake_artists) + 1,
+        "name" : artist.name,
+        "genre" : artist.genre,
+    }
+    fake_artists.append(new_artist)
+    return new_artist
